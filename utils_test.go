7c176c6353ce1ea9c55eb3d9f89cef1b436703b9
Maximilian Breida <maxbreida@gmail.com>
2025-02-04 13:43:37 +0100

    fix: fix golangci-lint

--- utils_test.go
+++ utils_test.go
@@ -3,8 +3,9 @@ package gocloak_test
 import (
 	"testing"
 
-	"github.com/Nerzal/gocloak/v13"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/Nerzal/gocloak/v13"
 )
 
 func TestStringP(t *testing.T) {
